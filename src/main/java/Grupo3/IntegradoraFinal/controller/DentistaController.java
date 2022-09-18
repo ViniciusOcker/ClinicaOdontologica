07ef46953e50d1a5f73f0b78a33911b654bf9d04
LAYASUARES <ladislaualexandre940@gmail.com>
2022-09-18 15:25:59 -0300

    Update Dentista Controller

--- src/main/java/Grupo3/IntegradoraFinal/controller/DentistaController.java
+++ src/main/java/Grupo3/IntegradoraFinal/controller/DentistaController.java
@@ -3,6 +3,7 @@ package Grupo3.IntegradoraFinal.controller;
 import Grupo3.IntegradoraFinal.entity.dto.CriarDentistaDTO;
 import Grupo3.IntegradoraFinal.entity.dto.DentistaDTO;
 import Grupo3.IntegradoraFinal.entity.dto.PacienteDTO;
+import Grupo3.IntegradoraFinal.exception.ResourceNotFoundException;
 import Grupo3.IntegradoraFinal.service.impl.DentistaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,18 +25,30 @@ public class DentistaController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getById(@PathVariable Long id){
-        return new ResponseEntity<>(dentistaService.getById(id), HttpStatus.OK);
+    public ResponseEntity<?> getById(@PathVariable Long id) throws ResourceNotFoundException {
+        try{
+            return new ResponseEntity<>(dentistaService.getById(id), HttpStatus.OK);
+        } catch(Exception e){
+            throw new ResourceNotFoundException("Não foi possivel localizar o id " + id + " de dentista!");
+        }
     }
 
     @GetMapping("/")
-    public ResponseEntity<?> getByAll(){
-        return new ResponseEntity<>(dentistaService.getByAll(), HttpStatus.OK);
+    public ResponseEntity<?> getByAll() throws ResourceNotFoundException {
+        try{
+            return new ResponseEntity<>(dentistaService.getByAll(), HttpStatus.OK);
+        } catch(Exception e){
+            throw new ResourceNotFoundException("Não foi possivel obter uma lista de Dentistas");
+        }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id){
-        return new ResponseEntity<>(dentistaService.delete(id), HttpStatus.OK);
+    public ResponseEntity<?> delete(@PathVariable Long id) throws ResourceNotFoundException {
+        try{
+            return new ResponseEntity<>(dentistaService.delete(id), HttpStatus.OK);
+        } catch(Exception e){
+            throw new ResourceNotFoundException("Não foi possivel deletar o id " + id + " de dentista");
+        }
     }
 
     @PutMapping("/{id}")
