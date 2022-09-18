81cc729dd833609eb2b1b2aa338c97c72752bcb8
LAYASUARES <ladislaualexandre940@gmail.com>
2022-09-18 15:18:03 -0300

    Adicionar exceptions

---
+++ src/main/java/Grupo3/IntegradoraFinal/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package Grupo3.IntegradoraFinal.exception;
+
+public class ResourceNotFoundException extends Exception {
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+
+
+}
