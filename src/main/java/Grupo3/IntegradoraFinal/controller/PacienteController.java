8eac2883afa7a1dba33232a3cd8324c925ce0380
Vinicius Ocker Fagundes <60163731+ViniciusOcker@users.noreply.github.com>
2022-09-19 22:50:26 -0300

    Ajustes (#28)
    * add dentista validation
    
    * add endereco validation
    
    * validation numero endereco
    
    * implemtando as expretions do endereco
    
    * ajustes do paciente
    
    * add validation paciente

--- src/main/java/Grupo3/IntegradoraFinal/controller/PacienteController.java
+++ src/main/java/Grupo3/IntegradoraFinal/controller/PacienteController.java
@@ -2,6 +2,7 @@ package Grupo3.IntegradoraFinal.controller;
 
 import Grupo3.IntegradoraFinal.entity.dto.CriarPacienteDTO;
 import Grupo3.IntegradoraFinal.entity.dto.PacienteDTO;
+import Grupo3.IntegradoraFinal.exception.BadRequestException;
 import Grupo3.IntegradoraFinal.exception.ResourceNotFoundException;
 import Grupo3.IntegradoraFinal.service.impl.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,30 +25,18 @@ public class PacienteController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getById(@PathVariable Long id) throws ResourceNotFoundException {
-        try{
-            return new ResponseEntity<>(pacienteService.getById(id), HttpStatus.OK);
-        } catch(Exception e){
-            throw new ResourceNotFoundException("Não foi possivel localizar o id " + id + " de paciente!");
-        }
+    public ResponseEntity<?> getById(@PathVariable Long id) throws Exception {
+        return new ResponseEntity<>(pacienteService.getById(id), HttpStatus.OK);
     }
 
     @GetMapping("/")
-    public ResponseEntity<?> getByAll() throws ResourceNotFoundException {
-        try{
-            return new ResponseEntity<>(pacienteService.getByAll(), HttpStatus.OK);
-        } catch(Exception e){
-            throw new ResourceNotFoundException("Não foi possivel obter uma lista de Paciente");
-        }
+    public ResponseEntity<?> getByAll() throws Exception {
+        return new ResponseEntity<>(pacienteService.getByAll(), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id) throws ResourceNotFoundException {
-        try{
-            return new ResponseEntity<>(pacienteService.delete(id), HttpStatus.OK);
-        } catch(Exception e){
-            throw new ResourceNotFoundException("Não foi possivel deletar o id " + id + " de paciente");
-        }
+    public ResponseEntity<?> delete(@PathVariable Long id) throws Exception {
+        return new ResponseEntity<>(pacienteService.delete(id), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
@@ -56,7 +45,7 @@ public class PacienteController {
     }
 
     @GetMapping("/search")
-    public ResponseEntity<?> buscarPeloNomeCompleto(@RequestParam(required = false, defaultValue = "") String nome, @RequestParam(required = false, defaultValue = "") String sobrenome){
+    public ResponseEntity<?> buscarPeloNomeCompleto(@RequestParam(required = false, defaultValue = "") String nome, @RequestParam(required = false, defaultValue = "") String sobrenome) throws Exception {
         return new ResponseEntity<>(pacienteService.findPaciente(nome, sobrenome), HttpStatus.OK);
     }
 }
