8c1b586a9deb58aaab1418fcf519bdb18d7ad1d6
Vinicius Ocker Fagundes <60163731+ViniciusOcker@users.noreply.github.com>
2022-09-21 18:50:16 -0300

    Add testes unitarios (#30)
    * Adicionar exceptions
    
    * Update da Consulta Controle
    
    * Update Dentista Controller
    
    * Update Paciente Controller
    
    * Início da implementação de testes unitários.
    
    * Criação dos testes iniciais para DentistaDTO e PacienteDTO
    
    Co-authored-by: LAYASUARES <ladislaualexandre940@gmail.com>
    Co-authored-by: robertoCastro <bkastro@gmail.com>

--- src/main/java/Grupo3/IntegradoraFinal/exception/GlobalException.java
+++ src/main/java/Grupo3/IntegradoraFinal/exception/GlobalException.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalException {
-
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> processarErrorNotFound(ResourceNotFoundException ex){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
