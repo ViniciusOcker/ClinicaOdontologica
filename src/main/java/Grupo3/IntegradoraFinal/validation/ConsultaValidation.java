912ecb72060726bf293f355cf08065099b09d1e6
Ualace36 <89046095+Ualace36@users.noreply.github.com>
2022-09-21 11:53:41 -0300

    adicionando validação de consulta parte 2

--- src/main/java/Grupo3/IntegradoraFinal/validation/ConsultaValidation.java
+++ src/main/java/Grupo3/IntegradoraFinal/validation/ConsultaValidation.java
@@ -9,11 +9,18 @@ import Grupo3.IntegradoraFinal.repository.IPacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Component
 public class ConsultaValidation {
 
     private static final int minimoMinutoConsulta = 15;
     private static final int maximoMinutoConsulta = 60;
+    private static final String dataRegexp = "^[0-9]{2}\\/[0-9]{2}\\/[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}$";
 
     @Autowired
     IConsultaRepository consultaRepository;
@@ -43,6 +50,32 @@ public class ConsultaValidation {
             return "Este Dentista não existe";
         }
         return null;
+    }
+
+    public String validationData (Long id, String inicio, String fim){
+
+        if(!(Evalido(inicio, dataRegexp) && Evalido(fim, dataRegexp))
+        ) {
+            return "A Data de início ou de fim  está no formato incorreto [Formato correto: dd/MM/yyyy HH:mm:ss]";
+        }
+        LocalDateTime inicioTime = LocalDateTime.parse(inicio, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+        LocalDateTime fimTime = LocalDateTime.parse(fim, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+        if(fimTime.isAfter(inicioTime)){
+            return "A data de fim não pode ser inferior a de início";
+        } else if (fimTime.until(inicioTime, ChronoUnit.MINUTES) < minimoMinutoConsulta) {
+            return "A duração da consulta é inferior ao tempo permitido";
+        } else if (fimTime.until(inicioTime, ChronoUnit.MINUTES) > maximoMinutoConsulta) {
+            return "A duração da consulta é superior ao tempo permitido";
+        } else if (!consultaRepository.getByConsultaData(id,inicioTime,fimTime).isEmpty()) {
+            return "Este período de tempo está ocupado";
+        }
+        return null;
+    }
+
+    public boolean Evalido(String texto, String regexp){
+        Pattern pattern = Pattern.compile(regexp, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(texto);
+        return matcher.find();
     }
 
 }
